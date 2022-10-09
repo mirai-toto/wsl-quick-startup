906194c89afdebb3623a939e7cf335eb61d535ef
mirai-toto <85745033+mirai-toto@users.noreply.github.com>
2022-10-09 23:13:13 +0200

    Initial commit

---
+++ scripts/InstallWingetPackages.ps1
@@ -0,0 +1,39 @@
+function InstallWingetPackages {
+    param (
+        [string[]]$PackagesToInstall
+    )
+
+    Write-Host "Installing packages using winget..."
+
+    # Check if winget is installed
+    if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
+        Write-Host "winget is not installed. Please install winget first."
+        "Error: winget not installed" | Out-File -FilePath $LOG_FILE -Append
+        exit 1
+    }
+
+    foreach ($packageId in $PackagesToInstall) {
+        $packageId = $packageId.Trim()
+        Write-Host "Processing package: $packageId"
+
+        # Check if package is already installed
+        $isInstalled = winget list --id $packageId -q
+        if ($isInstalled) {
+            Write-Host "Package $packageId is already installed."
+            continue
+        }
+
+        # Install the package
+        try {
+            winget install --id $packageId -e --accept-package-agreements --accept-source-agreements -h
+            if ($LASTEXITCODE -ne 0) {
+                throw "winget failed with exit code $LASTEXITCODE"
+            }
+            Write-Host "Package $packageId installed successfully."
+        } catch {
+            Write-Host "Failed to install package $packageId."
+            "Error: Failed to install package $packageId" | Out-File -FilePath $LOG_FILE -Append
+            exit 1
+        }
+    }
+}
